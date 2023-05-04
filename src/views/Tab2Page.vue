fd51f0b82eaf3a7bdc0879660a5294c025878d68
Edgar Osciel Romero Lezma <osciel.lezma@gmail.com>
2023-05-04 08:51:11 -0600

    cambio de titulo de la tab 2 (:

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Registro de Maestros</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
